763cd0e6f0a75ab39f48ca9a5ea76e158a7999d5
Vadzim_Kavalkou <vadzim_kavalkou@epam.com>
2022-05-15 17:35:56 +0400

    Datagen module added.

---
+++ datagen/src/main/java/by/vk/datagen/DatagenApplication.java
@@ -0,0 +1,13 @@
+package by.vk.datagen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatagenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatagenApplication.class, args);
+	}
+
+}
