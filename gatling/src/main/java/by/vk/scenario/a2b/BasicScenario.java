c333a6a49ab4d0d8f7b70eb704578f6c1550e929
Vadzim_Kavalkou <vadzim_kavalkou@epam.com>
2022-05-20 10:24:40 +0400

    Gatling is on fire.

---
+++ gatling/src/main/java/by/vk/scenario/a2b/BasicScenario.java
@@ -0,0 +1,12 @@
+package by.vk.scenario.a2b;
+
+import io.gatling.javaapi.core.*;
+import io.gatling.javaapi.http.*;
+
+import java.time.Duration;
+
+import static io.gatling.javaapi.core.CoreDsl.*;
+import static io.gatling.javaapi.http.HttpDsl.*;
+
+public class BasicScenario extends Simulation {
+}
