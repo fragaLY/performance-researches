f88ee17127d28a86edf34934770e672245c6dda0
Vadzim_Kavalkou <vadzim_kavalkou@epam.com>
2022-05-26 12:10:24 +0400

    Gatling fixes.

--- gatling/src/gatling/java/by/vk/scenario/a2b/A2BSimulation.java
+++ gatling/src/gatling/java/by/vk/scenario/a2b/A2BSimulation.java
@@ -3,6 +3,7 @@ package by.vk.scenario.a2b;
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
 import io.gatling.javaapi.http.HttpProtocolBuilder;
+import io.netty.handler.codec.http.HttpResponseStatus;
 
 import java.time.Duration;
 import java.util.Collections;
@@ -18,6 +19,7 @@ import static io.gatling.javaapi.core.CoreDsl.jsonPath;
 import static io.gatling.javaapi.core.CoreDsl.rampUsersPerSec;
 import static io.gatling.javaapi.core.CoreDsl.scenario;
 import static io.gatling.javaapi.http.HttpDsl.http;
+import static io.gatling.javaapi.http.HttpDsl.status;
 
 public class A2BSimulation extends Simulation {
     private static final Iterator<Map<String, Object>> FEEDER = Stream.generate((Supplier<Map<String, Object>>) () -> Collections.singletonMap("userId", ThreadLocalRandom.current().nextLong(1, 200_001))).iterator();
@@ -34,72 +36,106 @@ public class A2BSimulation extends Simulation {
             .feed(FEEDER)
             .exec(http("[GET] The list of available countries is presented.")
                     .get("/countries")
+                    .check(status().is(HttpResponseStatus.OK.code()))
                     .check(jsonPath("$[:].countryId").findRandom(1, true).saveAs("countryId"))
             )
+            .exitHereIfFailed()
+            .exec(session -> {
+                System.out.println(session.getString("countryId") + "; " + session.getString("cityOriginId"));
+                return session;
+            })
             .pause(1, 3)
             .exec(http("[GET] The list of available cities is presented.")
-                    .get("/countries/{countryId}/cities")
+                    .get("/countries/#{countryId}/cities")
+                    .check(status().is(HttpResponseStatus.OK.code()))
                     .check(jsonPath("$[:].cityId").findRandom(1, true).saveAs("cityOriginId"))
             )
+            .exec(session -> {
+                System.out.println(session.getString("countryId") + "; " + session.getString("cityOriginId"));
+                return session;
+            })
+            .exitHereIfFailed()
             .pause(1, 3)
-            .exec(http("[GET] The list of available origins is presented.")
-                    .get("/countries/{countryId}/cities/{cityOriginId}/locations")
-                    .check(jsonPath("$[:].locationId").findRandom(1, true).saveAs("originId"))
+            .exec(session -> {
+                        http("[GET] The list of available origins is presented.")
+                                .get("/countries/#{countryId}/cities/#{cityOriginId}/locations")
+                                .check(status().is(HttpResponseStatus.OK.code()))
+                                .check(jsonPath("$[:].locationId").findRandom(1, true).saveAs("originId"));
+                        return session;
+                    }
             )
             .pause(3, 5)
-            .exec(http("[GET] The list of available cities is presented.")
-                    .get("/countries/{countryId}/cities")
-                    .check(jsonPath("$[:].cityId").findRandom(1, true).saveAs("cityDestinationId"))
+            .exec(session -> {
+                        http("[GET] The list of available cities is presented.")
+                                .get("/countries/#{countryId}/cities")
+                                .check(status().is(HttpResponseStatus.OK.code()))
+                                .check(jsonPath("$[:].cityId").findRandom(1, true).saveAs("cityDestinationId"));
+                        return session.set("cityDestinationId", 2);
+                    }
             )
+            .exitHereIfFailed()
             .pause(1, 3)
             .exec(http("[GET] The list of available destinations is presented.")
-                    .get("/countries/{countryId}/cities/{cityDestinationId}/locations")
+                    .get("/countries/#{countryId}/cities/#{cityDestinationId}/locations")
+                    .check(status().is(HttpResponseStatus.OK.code()))
                     .check(jsonPath("$[:].locationId").findRandom(1, true).saveAs("destinationId"))
             )
+            .exitHereIfFailed()
             .pause(3, 5)
             .exec(http("[GET] The list of available transfers by selected origin, destination, and date is presented.")
                     .get("/transfers")
                     .queryParam("originId", "{originId}")
                     .queryParam("destinationId", "{destinationId}")
                     .queryParam("date", "1970-01-01")
+                    .check(status().is(HttpResponseStatus.OK.code()))
                     .check(jsonPath("$[:].transferId").findRandom(1, true).saveAs("transferId"))
             )
+            .exitHereIfFailed()
             .pause(3, 5)
             .exec(http("[POST] The transfer is booked in the system.")
-                    .post("/users/{userId}/transfers/{transferId}")
+                    .post("/users/#{userId}/transfers/#{transferId}")
+                    .check(status().is(HttpResponseStatus.CREATED.code()))
                     .body(StringBody("{ \"description\": \"My internal uuid is " + UUID.randomUUID() + "\" }"))
             )
             .pause(1, 3)
             .exec(http("[GET] The list of all my transfers (COMPLETED, CANCELED, BOOKED) is presented.")
-                    .get("/users/{userId}/transfers")
+                    .get("/users/#{userId}/transfers")
+                    .check(status().is(HttpResponseStatus.OK.code()))
                     .check(jsonPath("$[:].transfer.transferId").findRandom(1, true).saveAs("selectedUserTransferId"))
             )
+            .exitHereIfFailed()
             .pause(1, 3)
             .exec(http("[GET] One of the transfers (COMPLETED, CANCELED, BOOKED) is retrieved.")
-                    .get("/users/{userId}/transfers/{selectedUserTransferId}")
+                    .get("/users/#{userId}/transfers/#{selectedUserTransferId}")
+                    .check(status().is(HttpResponseStatus.OK.code()))
             )
             .pause(1, 5)
             .exec(http("[PUT] Any (BOOKED) transfer description is updated.")
-                    .put("/users/{userId}/transfers/{selectedUserTransferId}")
+                    .put("/users/#{userId}/transfers/#{selectedUserTransferId}")
+                    .check(status().is(HttpResponseStatus.NO_CONTENT.code()))
                     .body(StringBody("{\"state\": \"BOOKED\", \"description\": \"My new internal UUID " + UUID.randomUUID() + "\" }"))
             )
             .pause(1, 3)
             .exec(http("[PUT] Any (BOOKED) transfer is canceled (CANCELED).")
-                    .put("/users/{userId}/transfers/{selectedUserTransferId}")
+                    .put("/users/#{userId}/transfers/#{selectedUserTransferId}")
+                    .check(status().is(HttpResponseStatus.NO_CONTENT.code()))
                     .body(StringBody("{\"state\": \"CANCELED\", \"description\": \"My new internal UUID for canceled transfer " + UUID.randomUUID() + "\" }"))
             )
             .pause(1, 3)
             .exec(http("[PUT] Any (BOOKED) transfer is completed (COMPLETED).")
-                    .put("/users/{userId}/transfers/{selectedUserTransferId}")
+                    .put("/users/#{userId}/transfers/#{selectedUserTransferId}")
+                    .check(status().is(HttpResponseStatus.NO_CONTENT.code()))
                     .body(StringBody("{\"state\": \"COMPLETED\", \"description\": \"My new internal UUID for completed transfer " + UUID.randomUUID() + "\" }"))
             )
             .pause(1, 3)
             .exec(http("[GET] User is retrieved with her/his transfers data.")
-                    .post("/users/{userId}")
+                    .post("/users/#{userId}")
+                    .check(status().is(HttpResponseStatus.OK.code()))
             )
             .pause(1, 3)
             .exec(http("[PUT] User updates with her/his own data.")
-                    .post("/users/{userId}")
+                    .post("/users/#{userId}")
+                    .check(status().is(HttpResponseStatus.NO_CONTENT.code()))
                     .body(StringBody("{ \"firstName\": \" NewFirstName{userId} \", \"lastName\": \"NewLastName{userId}\" }"))
             )
             .pause(1, 5);
