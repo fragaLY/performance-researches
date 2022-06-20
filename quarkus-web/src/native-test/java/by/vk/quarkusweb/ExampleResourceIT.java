fb077b7fda920de5e542305439de353c031a7a69
Vadzim_Kavalkou <vadzim_kavalkou@epam.com>
2022-06-20 16:12:47 +0400

    Quarkus initial commit

---
+++ quarkus-web/src/native-test/java/by/vk/quarkusweb/ExampleResourceIT.java
@@ -0,0 +1,9 @@
+package by.vk.quarkusweb;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+public class ExampleResourceIT extends ExampleResourceTest {
+
+  // Execute the same tests but in native mode.
+}
