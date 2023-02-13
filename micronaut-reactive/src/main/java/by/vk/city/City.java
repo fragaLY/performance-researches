d1471523466f02b15eeffe5482aa559d387f1834
Vadzim_Kavalkou <vadzim_kavalkou@epam.com>
2023-02-13 14:01:10 +0300

    Cities Api completed.

--- micronaut-reactive/src/main/java/by/vk/city/City.java
+++ micronaut-reactive/src/main/java/by/vk/city/City.java
@@ -4,6 +4,7 @@ import by.vk.country.Country;
 import io.micronaut.data.annotation.GeneratedValue;
 import io.micronaut.data.annotation.Id;
 import io.micronaut.data.annotation.MappedEntity;
+import io.micronaut.data.annotation.Relation;
 import io.micronaut.serde.annotation.Serdeable;
 import java.util.Objects;
 
@@ -16,7 +17,41 @@ public class City {
   private Long id;
   private String name;
   private String code;
+
+  @Relation(Relation.Kind.MANY_TO_ONE)
   private Country country;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getCode() {
+    return code;
+  }
+
+  public void setCode(String code) {
+    this.code = code;
+  }
+
+  public Country getCountry() {
+    return country;
+  }
+
+  public void setCountry(Country country) {
+    this.country = country;
+  }
 
   @Override
   public boolean equals(Object o) {
@@ -42,30 +77,6 @@ public class City {
     result = 31 * result + (code != null ? code.hashCode() : 0);
     result = 31 * result + (country != null ? country.hashCode() : 0);
     return result;
-  }
-
-  public Long getId() {
-    return id;
-  }
-
-  public void setId(Long id) {
-    this.id = id;
-  }
-
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getCode() {
-    return code;
-  }
-
-  public void setCode(String code) {
-    this.code = code;
   }
 
   @Override
