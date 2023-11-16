069a8db759256133382db5e14840f81ea736e2c8
Vadzim_Kavalkou <vadzim_kavalkou@epam.com>
2023-11-16 17:19:22 +0300

    Spring Reactive (Native as well) are actualized.

---
+++ spring-boot-reactive-native/build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    java
+    application
+    id("org.springframework.boot") version "3.1.5"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("org.graalvm.buildtools.native") version "0.9.28"
+    id("com.google.cloud.tools.jib") version "3.4.0"
+}
+
+group = "by.vk"
+version = "1.0.0-RC1"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+springBoot {
+    buildInfo()
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    //region spring
+    implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    //endregion
+    //region logback
+    implementation("ch.qos.logback.contrib:logback-jackson:0.1.5")
+    implementation("ch.qos.logback.contrib:logback-json-classic:0.1.5")
+    //endregion
+    //region lombok
+    annotationProcessor("org.projectlombok:lombok")
+    implementation("org.projectlombok:lombok")
+    //endregion
+    //region postgres
+    implementation("io.r2dbc:r2dbc-postgresql:0.8.13.RELEASE")
+    //endregion
+}
+
+tasks.bootBuildImage {
+    buildpacks.set(listOf("gcr.io/paketo-buildpacks/java-native-image:latest"))
+    builder.set("paketobuildpacks/builder:tiny")
+    environment.set(mapOf("BP_NATIVE_IMAGE" to "true"))
+}
+
+jib {
+    to {
+        image = "srn-service:latest"
+    }
+    from {
+        image = "gcr.io/distroless/java17:latest"
+    }
+    container {
+        ports = listOf("8080")
+        labels.set(mapOf("appname" to application.applicationName, "version" to version.toString(), "maintainer" to "Vadzim Kavalkou <vadzim.kavalkou@gmail.com>"))
+        creationTime.set("USE_CURRENT_TIMESTAMP")
+    }
+}
